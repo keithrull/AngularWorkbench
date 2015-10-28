c9761e272cdb585f6f0cf6f129cf18cd681729b5
Keith Rull <keith.rull@gmail.com>
2015-10-28 14:23:01 -0700

    local commit

---
+++ AngularWorkbench/Controllers/SamplesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularWorkbench.Controllers
+{
+    public class SamplesController : Controller
+    {
+        // GET: Samples
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Example1()
+        {
+            return View();
+        }
+
+        public ActionResult Example2()
+        {
+            return View();
+        }
+
+        public ActionResult Example3()
+        {
+            return View();
+        }
+
+        public ActionResult Example4()
+        {
+            return View();
+        }
+
+        public ActionResult Example5()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularWorkbench/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularWorkbench.MvcApplication" Language="C#" %>
